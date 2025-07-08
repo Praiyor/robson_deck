6bb80d91d0b9ba34c6024ed6281b66d5d5954d0a
Ana Fábia Coelho <afcoelho44@gmail.com>
2025-07-08 16:26:39 -0300

    adicioando o prometheus

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { deckRoutes } from "./routes/deckRoutes.js";
+import client from "prom-client";
 
 const app = express();
 
@@ -7,6 +8,15 @@ const PORT = 4052;
 
 app.use(express.json());
 app.use('/decks', deckRoutes);
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
